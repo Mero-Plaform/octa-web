35c830dd3c58c14ee69e43a71b18fbee68fe746a
Sandra <sandra@bullok>
2023-05-13 23:04:27 +0300

    initial commit

---
+++ src/lib/Link1Handler.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+    import { InputChip } from '@skeletonlabs/skeleton';
+    import { writable } from 'svelte/store';
+
+    const word = {
+      arr: ['1', '2', '3']
+    };
+
+</script>
+
+<div class='flex justify-center items-center gap-5 h-screen w-screen'>
+  Link1 handler component
+  <img src="src/assets/svelte.svg" alt="svelte icon" />
+</div>
+
+<InputChip bind:value={word.arr} name="chips" placeholder="Enter any value..." />
