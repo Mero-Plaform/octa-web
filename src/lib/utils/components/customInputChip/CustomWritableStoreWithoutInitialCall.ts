f89734d305f531aa3146b5dc797ca9efe98d4161
Sandra <sandra@bullok>
2023-05-30 02:06:05 +0300

    added new custom store

---
+++ src/lib/utils/components/customInputChip/CustomWritableStoreWithoutInitialCall.ts
@@ -0,0 +1,15 @@
+import { CustomWritableStore, type Callback } from './CustomStoreFactory.js';
+
+export class CustomWritableStoreWithoutInitialCall<T> extends CustomWritableStore<T>  {
+  subscribe(callback: Callback<T>) {
+    this.subscriptions.push(callback);
+    return () => this.unsubscribe(callback);
+  }
+}
+
+
+export const CustomWritableStoreWithoutInitialCallFactory = <T, I = unknown>(initialValue?: T, storeInterface?: I) => {
+  const store = new CustomWritableStoreWithoutInitialCall<T>(initialValue as T);
+  Object.assign(store, storeInterface);
+  return store as CustomWritableStoreWithoutInitialCall<T> & I;
+};
