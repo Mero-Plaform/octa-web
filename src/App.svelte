f488e6f3ab5fc7e430de959af2f7bec85f1ef9d1
Sandra <sandra@bullok>
2023-05-31 16:23:14 +0300

    fix: imports

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,10 @@
     Modal,
   } from "@skeletonlabs/skeleton";
   import Menu from "./lib/AppMenu/Menu.svelte";
-  import Router from "/src/lib/router/Router.svelte";
   import Dictionary from "./lib/dictionary/components/Dictionary.svelte";
-  import Loading from "./lib/shared/Loading.svelte";
   import Popups from "./lib/shared/components/Popups.svelte";
   import { modalComponentRegistry } from "./lib/shared/modalComponentRegistry.js";
+  import Router from './lib/router/Router.svelte';
 </script>
 
 <Menu/>
@@ -18,7 +17,4 @@
 
 <Router />
 
-<Loading />
-
 <Popups />
- 
