9b6c6fb253a12fc6d66c3bbb5e8876075e0aa6b7
Sandra <sandra@bullok>
2023-06-03 20:04:51 +0300

    added shuffleArr to helpers

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -1,3 +1,5 @@
 export const getIconMaskStyes = (iconSrc: string) => {
   return `-webkit-mask: url(${iconSrc}) no-repeat center / contain; mask: url(${iconSrc}) no-repeat center / contain`;
 };
+
+export const shuffleArr = <T = unknown>(arr: T[] | IterableIterator<T>) => [...arr].sort(() => Math.random() - 0.5);
