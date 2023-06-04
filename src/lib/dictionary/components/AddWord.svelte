d8b3d25dbbab9a4c10058550ad39905ff54c1dfa
Sandra <sandra@bullok>
2023-06-04 20:06:39 +0300

    improved addWord

--- src/lib/dictionary/components/AddWord.svelte
+++ src/lib/dictionary/components/AddWord.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
   import { modalStore, type ModalSettings } from "@skeletonlabs/skeleton";
   import { onDestroy, onMount } from "svelte";
-  import { getIconMaskStyes } from '/src/lib/utils/helpers.js';
+  import { getIconMaskStyes } from "../../utils/helpers.js";
 
   const modal: ModalSettings = {
     type: "component",
     component: "addWord",
-    backdropClasses: '!bg-cyan-200 !bg-opacity-50 backdrop-blur-sm',
+    backdropClasses: "!bg-cyan-200 !bg-opacity-50 backdrop-blur-sm",
   };
 
   const checkIsOpen = () => {
-    if ($modalStore.length === 0 ) { return false; }
-    return $modalStore.find((modals: { type: string, component: string }) => {
-      if (modals.type !== 'component') { return false; }
+    if ($modalStore.length === 0) {
+      return false;
+    }
+    return $modalStore.find((modals: { type: string; component: string }) => {
+      if (modals.type !== "component") {
+        return false;
+      }
       return modal.component === "addWord";
     });
   };
 
   const openModal = () => {
-    if (checkIsOpen()) { return; }
+    if (checkIsOpen()) {
+      return;
+    }
     modalStore.trigger(modal);
   };
 
@@ -41,6 +47,13 @@
   onDestroy(unsetKeyboardListeners);
 </script>
 
-<button on:dragstart|preventDefault on:click={openModal} class="btn h-10 w-10 p-2 bg-cyan-400 hover:bg-cyan-500 hover:filter-none rounded-md active:scale-90 active:bg-cyan-500 focus:bg-cyan-500 active:filter-none outline-none"> 
-  <div class="h-full w-full bg-white" style={getIconMaskStyes("/src/assets/icons/plus.svg")}></div>
+<button
+  on:dragstart|preventDefault
+  on:click={openModal}
+  class="btn h-10 w-10 p-2 bg-cyan-400 hover:bg-cyan-500 hover:filter-none rounded-md active:scale-90 active:bg-cyan-500 focus:bg-cyan-500 active:filter-none outline-none"
+>
+  <div
+    class="h-full w-full bg-white"
+    style={getIconMaskStyes("/src/assets/icons/plus.svg")}
+  />
 </button>
