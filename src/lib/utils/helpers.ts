9d80b67f86f04deb1e5550060010cfc8ae5b551a
Sandra <sandra@bullok>
2023-06-04 20:07:14 +0300

    added new helpers

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -2,4 +2,14 @@ export const getIconMaskStyes = (iconSrc: string) => {
   return `-webkit-mask: url(${iconSrc}) no-repeat center / contain; mask: url(${iconSrc}) no-repeat center / contain`;
 };
 
-export const shuffleArr = <T = unknown>(arr: T[] | IterableIterator<T>) => [...arr].sort(() => Math.random() - 0.5);
+/**
+ * shuffle data to new array (not deep copy)
+ */
+export const shuffle = <T = unknown>(data: T[] | IterableIterator<T>) => [...data].sort(() => Math.random() - 0.5); 
+
+export function* createIteratorFromArr<T = unknown>(arr: T[]) {
+  const length = arr.length;
+  for (let curItemIndex = 0; curItemIndex < length; curItemIndex += 1) {
+    yield arr[curItemIndex];
+  }
+}
