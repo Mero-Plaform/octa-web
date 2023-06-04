d90c33713c3fb5cde6dff5f822e0b6bbaa0ffc6a
Sandra <sandra@bullok>
2023-06-04 20:04:57 +0300

    added DB practice table & utils

--- src/lib/DB/AppDB.ts
+++ src/lib/DB/AppDB.ts
@@ -1,14 +1,17 @@
 
 import Dexie, { type Table } from 'dexie';
 import type { Word } from '../dictionary/interfaces/Word.js';
+import type { SettingsStore } from '../practice/interfaces/settings.js';
 
 export class AppDB extends Dexie {
   dictionary!: Table<Word>;
+  practice!: Table<SettingsStore>;
 
   constructor() {
     super('AppDB');
     this.version(1).stores({
-      dictionary: 'id'
+      dictionary: 'id',
+      practice: '++id'
     });
   }
 }
--- src/lib/DB/utils.ts
+++ src/lib/DB/utils.ts
@@ -1,4 +1,5 @@
 import type { Word } from '../dictionary/interfaces/Word.js';
+import type { SettingsStore } from '../practice/interfaces/settings.js';
 import { appDB } from './AppDB.js';
 
 export const onAddWord = (newWord: Word) => {
@@ -15,6 +16,24 @@ export const onDeleteWord = (wordId: Word['id']) => {
   appDB.dictionary.delete(wordId);
 };
 
-export const getAllDictionaryAsArray = () => {
+export const getDictionaryDataAsArray = () => {
   return appDB.dictionary.toArray();
+};
+
+export const getPracticeData = () => {
+  return appDB.practice.where({ id: 1 }).first();
+};
+
+export const initPracticeData = (practiceInitialValues: SettingsStore) => {
+  appDB.practice.add({
+    ...practiceInitialValues
+  });
+};
+
+/**
+ * the only key containing data in practice table
+ */
+const practiceDataKey = 1;
+export const updatePracticeData = (practiceData: SettingsStore) => {
+  appDB.practice.update(practiceDataKey, practiceData);
 };
