3416fe95bcba5c99d0a50e5bc2b18223758cb44e
Sandra <sandra@bullok>
2023-06-06 23:25:38 +0300

    removed extra stores

--- src/lib/practice/stores/practiceViewStateStore.ts
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-type PracticeViewStateStoreType = "config" | "task" | "loading";
-
-export const practiceViewStateStore = writable<PracticeViewStateStoreType>("config")
--- src/lib/practice/stores/taskDataStore.ts
+++
@@ -1,4 +0,0 @@
-import { writable } from 'svelte/store';
-import type { Word } from '../../dictionary/interfaces/Word.js';
-
-export const taskDataStore = writable<Word[]>([]);
