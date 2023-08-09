a6407745de9f2b9e1619681a74d79ef72367f8da
Sandra <sandra@bullok>
2023-08-09 17:59:10 +0300

    improved vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "./",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, './index.html'),
+      },
+      output: [
+        {
+          name: "main",
+          dir: "dist",
+        },
+        {
+          name: "main",
+          dir: "../dictionary-electron/src/renderer/main",
+        }
+      ]
+    },
+  },
 })
