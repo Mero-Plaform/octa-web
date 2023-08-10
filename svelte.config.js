1ebe20afb0a0b7e5e9d3a42525dc372138731fca
Sandra <sandra@bullok>
2023-08-10 14:22:32 +0300

    remove: comments

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,6 @@ import preprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
   preprocess: [
     vitePreprocess(),
     preprocess({
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   base: "./",
