ba1f8a65eda1c646578c2e7320993b803f32f555
Sandra <sandra@bullok>
2023-08-19 16:26:37 +0300

    update ipcUtils.ts: fix passive practice to statistic

--- src/lib/shared/desktopAppBuild/ipcUtils.ts
+++ src/lib/shared/desktopAppBuild/ipcUtils.ts
@@ -1,11 +1,14 @@
 import type { Word } from '../../pages/dictionary/interfaces/Word.js';
 import { wordStore } from '../../pages/dictionary/stores/wordStore.js';
+import { practiceActionStore } from '../../pages/practice/stores/practiceActionStore.js';
 
 const onNewPassivePracticeComplete = (_: unknown, result: boolean, wordId: Word["id"]) => {
   if (result) {
     wordStore.onSuccessfulPractice(wordId);
+    practiceActionStore.set("successful");
   } else {
     wordStore.onUnsuccessfulPractice(wordId);
+    practiceActionStore.set("unsuccessful");
   }
 };
 
