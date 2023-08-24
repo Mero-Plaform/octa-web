058ff170b5fb367a14a4f45eec0edf8f31e428a5
Sandra <sandra@bullok>
2023-08-24 22:15:06 +0300

    update appRootStyles.js and Header.svelte

--- plugins/vite/HTMLPlugin/appRootStyles.js
+++ plugins/vite/HTMLPlugin/appRootStyles.js
@@ -2,5 +2,4 @@
 /*                            used in desktop build                           */
 /* -------------------------------------------------------------------------- */
 
-export const appRootStyles = `style="margin-top: 24px; height: calc(100vh - 24px); border-bottom-right-radius: 0.375rem;
-  border-bottom-left-radius: 0.375rem;"`;
+export const appRootStyles = `style="margin-top: 24px; height: calc(100vh - 24px);"`;
--- src/lib/shared/components/AppDesktopHeader/Header.svelte
+++ src/lib/shared/components/AppDesktopHeader/Header.svelte
@@ -4,7 +4,7 @@
   import { sendToIpcMain } from "../../desktopAppBuild/ipcUtils.js";
 </script>
 
-<div class="fixed top-0 left-0 h-6 w-full flex bg-emerald-500 rounded-t-md">
+<div class="fixed top-0 left-0 h-6 w-full flex bg-emerald-500">
   <div class="dragZone grow" />
   <button
     on:click={() => sendToIpcMain("minimizeMainWindow")}
@@ -25,7 +25,7 @@
   </button>
   <button
     on:click={() => sendToIpcMain("closeMainWindow")}
-    class="btn p-1 h-6 w-10 text-white hover:bg-red-500 active:scale-100 [&_img]:active:scale-90 rounded-tr-md"
+    class="btn p-1 h-6 w-10 text-white hover:bg-red-500 active:scale-100 [&_img]:active:scale-90"
   >
     <img
       class="h-full"
