1d13be1c3d66ddecbcd4f12dc45ab1813e28d71f
Sandra <sandra@bullok>
2023-08-24 22:36:06 +0300

    create 1 file and update 1 file

--- src/lib/shared/components/AppDesktopHeader/Header.svelte
+++ src/lib/shared/components/AppDesktopHeader/Header.svelte
@@ -1,7 +1,31 @@
 <script lang="ts">
   import CrossIconURL from "../../../../assets/icons/cross-white.svg";
   import FullscreenIconURL from "../../../../assets/icons/fullscreen-white.svg";
+  import UnmaximizeIconURL from "../../../../assets/icons/unmaximize-white.svg";
   import { sendToIpcMain } from "../../desktopAppBuild/ipcUtils.js";
+
+  let maximizeBtnState: "maximize" | "unmaximize" = "maximize";
+
+  const maximizeBtnData = {
+    maximize: {
+      onClick() {
+        sendToIpcMain("maximizeMainWindow");
+      },
+      icon: FullscreenIconURL,
+    },
+    unmaximize: {
+      onClick() {
+        sendToIpcMain("unmaximizeMainWindow");
+      },
+      icon: UnmaximizeIconURL,
+    }
+  };
+
+  const onMaximizeBtnClick = () => {
+    maximizeBtnData[maximizeBtnState].onClick();
+    maximizeBtnState = maximizeBtnState === "maximize" ? "unmaximize" : "maximize";
+
+  };
 </script>
 
 <div class="fixed top-0 left-0 h-6 w-full flex bg-emerald-500">
@@ -10,16 +34,16 @@
     on:click={() => sendToIpcMain("minimizeMainWindow")}
     class="btn p-1 h-6 w-10 text-white hover:bg-emerald-500 active:scale-100 [&_img]:active:scale-90"
   >
-    <span class="-translate-y-1">__</span>
+    <span class="h-0.5 w-3 bg-white translate-y-1 rounded-md"></span>
   </button>
   <button
-    on:click={() => sendToIpcMain("maximizeMainWindow")}
+    on:click={onMaximizeBtnClick}
     class="btn p-1 h-6 w-10 text-white hover:bg-emerald-500 active:scale-100 [&_img]:active:scale-90"
   >
     <img
       class="h-full"
       on:dragstart|preventDefault
-      src={FullscreenIconURL}
+      src={maximizeBtnData[maximizeBtnState].icon}
       alt="fullscreen icon"
     />
   </button>
