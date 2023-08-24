053ce46dedded1416491a87ee52c9bde556cd6ae
Sandra <sandra@bullok>
2023-08-24 21:58:25 +0300

    update index.html and appRootStyles.js

--- plugins/vite/HTMLPlugin/appRootStyles.js
+++ plugins/vite/HTMLPlugin/appRootStyles.js
@@ -2,4 +2,5 @@
 /*                            used in desktop build                           */
 /* -------------------------------------------------------------------------- */
 
-export const appRootStyles = `style="margin-top: 24px; height: calc(100vh - 24px)"`;
+export const appRootStyles = `style="margin-top: 24px; height: calc(100vh - 24px); border-bottom-right-radius: 0.375rem;
+  border-bottom-left-radius: 0.375rem;"`;
