f54b282ca0e0eaa9b1fb2d2aa609ef70f6c8d10b
Sandra <sandra@bullok>
2023-09-04 20:44:08 +0300

    update TaskProgress.svelte

--- src/lib/pages/practice/components/TaskProgress.svelte
+++ src/lib/pages/practice/components/TaskProgress.svelte
@@ -3,6 +3,7 @@
     practiceProgressStore,
     type PracticeProgressData,
   } from "../stores/practiceProgressStore.js";
+    import { settingsStore } from '../stores/settingsStore.js';
 
   export let wrapStyleClasses = "";
   let successCount: number;
@@ -40,13 +41,10 @@
   <span class={`text-3xl ${successCountStyles}`}>
     {successCount}
   </span>
+  {#if $settingsStore.practiceType === "section"}
+    <span class="text-sm text-slate-400 ml-7">current:</span>
+    <span class="text-3xl text-slate-500">{$practiceProgressStore.wordIds.length}</span>
+  {/if}
   <span class="text-sm text-slate-400 ml-7">total:</span>
   <span class="text-3xl text-slate-500">{totalCount}</span>
-  <!-- <div class="mt-5 flex flex-wrap justify-center h-10 gap-1 w-full">
-    {#each $practiceProgressStore.stats as stat}
-      <span
-        class={`basis-3 h-3 ${stat ? "bg-slate-400" : "bg-red-400"} rounded-sm`}
-      />
-    {/each}
-  </div> -->
 </div>
