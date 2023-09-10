feb6386dc44132d52fb959d3a5f7536a9710903e
Sandra <sandra@bullok>
2023-09-10 13:59:49 +0300

    update ipcUtils.ts

--- src/lib/shared/desktopAppBuild/ipcUtils.ts
+++ src/lib/shared/desktopAppBuild/ipcUtils.ts
@@ -2,6 +2,7 @@ import type { Word } from '../../pages/dictionary/interfaces/Word.js';
 import { wordStore } from '../../pages/dictionary/stores/wordStore.js';
 import type { PracticeTarget } from '../../pages/practice/interfaces/settings.js';
 import { onPassivePractice } from '../../pages/statistic/stores/statisticStore/statisticStoreUtils.js';
+import routerStore from '../../router/routerStore.js';
 import { setMaximizeStateStoreValue, type MaximizeStates } from '../components/AppDesktopHeader/maximizeStateStore.js';
 
 const onNewPassivePracticeComplete = (_: unknown, result: boolean, wordId: Word["id"], practiceTarget: PracticeTarget) => {
@@ -17,6 +18,7 @@ const onNewPassivePracticeComplete = (_: unknown, result: boolean, wordId: Word[
 export const initDesktopActionListeners = () => {
   window.electron?.ipcRenderer.on("newPassivePracticeComplete", onNewPassivePracticeComplete);
   window.electron?.ipcRenderer.on("setMaximizeStateStoreValue", (_, state: MaximizeStates) => setMaximizeStateStoreValue(state));
+  window.electron?.ipcRenderer.on("openPracticePage", () => routerStore.set("/practice"));
 };
 
 export const sendToIpcMain = (channel: string, ...data: unknown[]) => {
