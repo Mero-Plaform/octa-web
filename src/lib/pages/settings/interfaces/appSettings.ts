66189025874a3ab01cdfeb235a0d399bc531168c
Sandra Bullock <Sandra@Bullock>
2023-12-26 22:59:35 +0200

    add: DB IdleModeSettings type in AppSettings

--- src/lib/pages/settings/interfaces/appSettings.ts
+++ src/lib/pages/settings/interfaces/appSettings.ts
@@ -1,8 +1,10 @@
 import type { BasicSettings } from './basicSettings.js';
+import type { IdleModeSettings } from "./idleModeSettings.js";
 import type { PracticeSettings } from './practice.js';
 
 export type AppSettings = {
   basic: BasicSettings;
+  idleMode: IdleModeSettings;
   practice: {
     passive: PracticeSettings;
     active: PracticeSettings;
