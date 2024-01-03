8006a10ac25a4a0a3797207bbdb760b33bd0071a
Sandra Bullock <Sandra@Bullock>
2024-01-03 20:14:14 +0200

    improve: dev/build modes & updated svelty-picker & installed svelte-carousel

--- prebuild-setup/desktop.ts
+++
@@ -1,7 +0,0 @@
-import { writeFile } from 'node:fs/promises';
-import { ENV_FILE_PATH, getEnvBuildVariable } from './general';
-
-const PLATFORM = "desktop";
-
-writeFile(ENV_FILE_PATH, getEnvBuildVariable(PLATFORM))
-  .catch(err => console.log(err));
